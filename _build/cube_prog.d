2f8c9047265e9fc2da480c51e4ffa18195aab1f7
Charlie West-Taylor <cwt1g14@soton.ac.uk>
2016-04-28 13:23:30 +0100

    Just nonsense stuff to try and figure out drawing

--- _build/cube_prog.d
+++ _build/cube_prog.d
@@ -1,5 +1,7 @@
-_build/cube_prog.o: cube_prog.c lcd/lcd.h lcd/svgrgb565.h
+_build/cube_prog.o: cube_prog.c lcd/lcd.h lcd/svgrgb565.h lcd/ili934x.h
 
 lcd/lcd.h:
 
 lcd/svgrgb565.h:
+
+lcd/ili934x.h:
--- cube_prog.c
+++ cube_prog.c
@@ -1,13 +1,51 @@
 #include "lcd.h"
+#include "ili934x.h"
 
 void main(void) {
 	/* Initialise the LCD display driver */
 	init_lcd();
 
 	/* INITIAL TEST: RECTANGLE. */
-	/* Placeholder rectangle, for testing. Args: left, right, top, bottom. */
-	rectangle r = {0, 50, 0, 50};
+	/* Placeholder rectangles, for testing. Args: left, right, top, bottom. */
+	/*rectangle r1 = {0, 5, 0, 5};*/
+	rectangle r2 = {1, 5, 1, 5};
 
-	/* Drawing the rectangle. Args: Rectangle, colour. */
-	fill_rectangle(r, FIRE_BRICK);
+	/* Drawing the rectangles. Args: Rectangle, colour. */
+	/*fill_rectangle(r1, BLACK);*/
+	fill_rectangle(r2, BLACK);
+/*	write_data16(CYAN);
+	write_data16(BLUE);
+	write_data16(PURPLE);
+	write_data16(RED);
+	write_data16(ORANGE);
+	write_data16(YELLOW);
+	write_data16(GREEN_YELLOW);
+	write_data16(LIME_GREEN);
+	write_data16(GREEN);
+	write_data16(TURQUOISE);*/
+	write_data16(CYAN);
+	write_data16(CYAN);
+	write_data16(CYAN);
+	write_data16(CYAN);
+	write_data16(CYAN);
+	write_data16(RED);
+	write_data16(RED);
+	write_data16(RED);
+	write_data16(RED);
+	write_data16(RED);
+	write_data16(GREEN);
+	write_data16(GREEN);
+	write_data16(GREEN);
+	write_data16(GREEN);
+	write_data16(GREEN);
+	write_data16(PURPLE);
+	write_data16(PURPLE);
+	write_data16(PURPLE);
+	write_data16(PURPLE);
+	write_data16(PURPLE);
+	write_data16(YELLOW);
+	write_data16(YELLOW);
+	write_data16(YELLOW);
+	write_data16(YELLOW);
+	write_data16(YELLOW);
 }
